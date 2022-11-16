d682a9714087069e2f74553f7368662e887bc6d5
zsheperdigian <zsheperdigian@gmail.com>
2022-11-16 13:41:01 -0800

    addded footer, hader, navbar intro code

---
+++ react_portfolio/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Footer() {
+
+    return(
+        <div>
+            <a>GitHub</a>
+            <a>LinkedIn</a>
+            
+        </div>
+
+    );
+}
+
+
+export default Footer;
---
+++ react_portfolio/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+function Header() {
+    //
+    return(
+        <header className="header">
+      <h1>Shep Sheperdigian</h1>
+        <ul> About Me</ul>
+        <ul> Portfolio</ul>
+        <ul> Contact</ul>
+        <ul> Resume </ul>
+        
+
+
+    </header>
+
+
+    )
+
+
+}
+
+
+export default Header
