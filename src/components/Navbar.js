1a1af7e1216480311e217ea2d92b8aa0307825b5
zsheperdigian <zsheperdigian@gmail.com>
2022-11-18 14:10:28 -0800

    refixed my issue of  my stuff not showing up..... added css for bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavItem from 'react-bootstrap/NavItem';
+
 import {
   NavLink
 } from 'react-router-dom';
@@ -14,13 +16,14 @@ function Navigationbar() {
   return (
     <Navbar bg="light" expand="lg">
     <Container fluid>
-      <Navbar.Brand href="/">Shep Sheperdigian</Navbar.Brand>
+      <Navbar.Brand>Shep Sheperdigian</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      
       <Navbar.Collapse id="basic-navbar-nav">
+        <Nav pullright ="true" className="me-auto">
+        <NavItem> <NavLink to='/'>About Me</NavLink> </NavItem>
         
-        <Nav className="me-auto">
-        <NavLink to='/' styles={({isActive}) => ({ color: !isActive ? "green" : "blue" })}>About Me</NavLink>
-          <NavLink to='/portfolio' styles={({isActive}) => ({ color: !isActive ? "green" : "blue"})}>Portfoilio</NavLink>
+          <NavLink to='/portfolio'>Portfoilio</NavLink>
           <NavLink to='/contact'>Contact</NavLink>
          <NavLink to='/resume'>Resume</NavLink>
         </Nav>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-
+//importing css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
