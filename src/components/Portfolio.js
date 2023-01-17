84ea1547f477c58303c8f365aecf37ac47266838
zsheperdigian <zsheperdigian@gmail.com>
2023-01-16 16:12:56 -0800

    added more styling to porfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,6 +8,7 @@ import PWGpic from './PWGpic.jpg';
 import Weatherpic from './Weatherpic.png';
 import WLYDOpic from './WLYDOpic.png';
 import GoModepic from './GoModepic.png';
+import "./style.css"
 
 const styles = {
     main: {
@@ -16,9 +17,16 @@ const styles = {
 
     },
     card: {
-        width: '50%'
-    }
+        width: '50%',
+    },
+    cardTop: {
+        height: '80%'
 
+    },
+    cardBottom: {
+        height: 'auto'
+
+    },
 }
 
 function Portfolio() {
@@ -26,73 +34,73 @@ function Portfolio() {
     return (
         <Container style={styles.main}>
             <Card style={styles.card}>
-                <Card.Body>
+                <Card.Body style={styles.cardTop}>
                     <Card.Title>Whole Let the Dawg Out</Card.Title>
-                    <Card.Img variant="top" src={WLYDOpic} alt ="" />
+                    <Card.Img variant="top" src={WLYDOpic} alt ="picture of Who Let your Dawg Out app" />
                 </Card.Body>
 
-                <Card.Body>
+                <Card.Body style={styles.cardBottom}>
                     <Card.Link href="https://sobe1290.github.io/wholetmydaWgout/">Deployed App Link</Card.Link>
                     <Card.Link href="https://github.com/sobe1290/wholetmydaWgout">GitHub Repo Link</Card.Link>
                 </Card.Body>
             </Card>
            
             <Card style={styles.card}>
-                <Card.Body>
+                <Card.Body style={styles.cardTop}>
                     <Card.Title>Nerd Knowledge Quiz</Card.Title>
                     <Card.Img variant="top" src={NerdquizPhoto} alt="picture of Nerd Quiz App" />
                 </Card.Body>
 
-                <Card.Body>
+                <Card.Body style={styles.cardBottom}>
                     <Card.Link href="https://sheltered-shore-88493.herokuapp.com/">Deployed App Link</Card.Link>
                     <Card.Link href="https://github.com/zshep/Nerd_Quiz">GitHub Repo Link</Card.Link>
                 </Card.Body>
             </Card>
            
             <Card style={styles.card}>
-                <Card.Body>
+                <Card.Body style={styles.cardTop}>
                     <Card.Title>Go Mode</Card.Title>
                     <Card.Img variant="top" src= {GoModepic} />
                 </Card.Body>
 
-                <Card.Body>
+                <Card.Body style={styles.cardBottom}>
                     <Card.Link href="https://gomodez.herokuapp.com/">Deployed App Link</Card.Link>
                     <Card.Link href="https://github.com/zshep/Fortress_of_Solitude">GitHub Repo Link</Card.Link>
                 </Card.Body>
             </Card>
 
             <Card style={styles.card}>
-                <Card.Body>
+                <Card.Body style={styles.cardTop}>
                     <Card.Title>Note Taker</Card.Title>
                     <Card.Img variant="top" src={NTpic} />
                 </Card.Body>
 
-                <Card.Body>
+                <Card.Body style={styles.cardBottom}>
                     <Card.Link href="https://murmuring-sea-43903.herokuapp.com/notes">Deployed App Link</Card.Link>
                     <Card.Link href="https://github.com/zshep/Note-taker">GitHub Repo Link</Card.Link>
                 </Card.Body>
             </Card>
 
             <Card style={styles.card}>
-                <Card.Body>
+                <Card.Body style={styles.cardTop}>
                     <Card.Title>Weather App</Card.Title>
                     <Card.Img variant="top" src={Weatherpic}  />
                 </Card.Body>
 
-                <Card.Body>
+                <Card.Body style={styles.cardBottom}>
                     <Card.Link href="https://github.com/zshep/weather_app">Deployed App Link</Card.Link>
                     <Card.Link href="https://github.com/zshep/weather_app">GitHub Repo Link</Card.Link>    
                 </Card.Body>
             </Card>
 
             <Card style={styles.card}>
-                <Card.Body>
+                <Card.Body style={styles.cardTop}>
                     <Card.Title>Password Generator</Card.Title>
                     <Card.Img variant="top" src={PWGpic}  />
                     
                 </Card.Body>
 
-                <Card.Body>
+                <Card.Body style={styles.cardBottom}>
                     <Card.Link href="https://zshep.github.io/weather_app/">Deployed App Link</Card.Link>
                     <Card.Link href="https://github.com/zshep/password_generator">GitHub Repo Link</Card.Link>
                 </Card.Body>
