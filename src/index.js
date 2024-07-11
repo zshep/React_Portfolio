0d8232c92c623ba54c83d3579e74960125163ccb
zsheperdigian <zsheperdigian@gmail.com>
2024-07-11 12:47:51 -0700

    made some changes to footing styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 import App from './App';
-//importing css
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
